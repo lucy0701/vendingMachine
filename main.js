5538f1c4956dfa00d31a732b70450f49d51dfa95
lucy0701 <sj8871@gmail.com>
2023-10-05 14:29:02 +0900

    완성

--- main.js
+++ main.js
@@ -2,10 +2,8 @@
 
 import Model from "./src/model.js";
 import View from "./src/view.js";
-import Service from "./src/service.js";
 import Controller from "./src/controller.js";
 
 const model = new Model;
 const view = new View;
-const service = new Service;
-const controller = new Controller (model, view, service);
+const controller = new Controller (model, view);
